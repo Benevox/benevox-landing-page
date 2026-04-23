b133adf6d0966908bd8ed7c169a13008c0bf924d
Peter O. Eno <70982909+Regicyber@users.noreply.github.com>
2026-04-23 16:45:33 +0100

    Change base path configuration to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Set the base path to match your GitHub repository name
-  base: "/benevox-landing-page/",
+  base: "/", // Set this to root
   server: {
     host: "::",
     port: 8080,
